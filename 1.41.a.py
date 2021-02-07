86a9ede176a1bf0efacbeec94fa92ec709da992a
Grencione <76771267+Grencione@users.noreply.github.com>
2021-02-07 22:55:08 +0200

    Update 1.41.a.py
    Tika pierakstīts komentārs rindā Nr.4

--- 1.41.a.py
+++ 1.41.a.py
@@ -1,4 +1,4 @@
 from math import * #izsaukt mat.funkcijas
 x=float(input("Ievadi x - "))
 
-print("Kvadratsakne no x ir - %.2f" % (sqrt(x)))
+print("Kvadratsakne no x ir - %.2f" % (sqrt(x)))# izvadit rezultatu uz ekrana
